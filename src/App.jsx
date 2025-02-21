2f023f448d02fc2245d7f6ea05071effc9cdcae2
David Palomino <david.leandro.palomino.rivera@gmail.com>
2025-02-21 00:54:32 -0500

    [Fix] App import Header path

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './assets/styles/App.css'
 import { Footer } from './components/common/Footer/Footer'
-import { Header } from './components/common/header/Header'
+import { Header } from './components/common/Header/Header'
 import { CTA } from './components/sections/CTA/CTA'
 import { HeroSection } from './components/sections/HeroSection/HeroSection'
 import { HowItWorks } from './components/sections/HowItWorks/HowItWorks'
