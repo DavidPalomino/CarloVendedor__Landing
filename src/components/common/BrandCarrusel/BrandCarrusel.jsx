10369bc195168c493a2de7839a15949ae4a27d68
David Palomino <david.leandro.palomino.rivera@gmail.com>
2025-02-26 15:46:26 -0500

    [Fix] Correcion de sizes y spacing

--- src/components/common/BrandCarrusel/BrandCarrusel.jsx
+++ src/components/common/BrandCarrusel/BrandCarrusel.jsx
@@ -1,8 +1,20 @@
 import styles from './BrandCarrusel.module.css'
-import placeHolder from '../../../assets/images/Volkswagen.svg'
+import Audi from '../../../assets/images/brands/Audi.svg'
+import BMW from '../../../assets/images/brands/BMW.svg'
+import Chevrolet from '../../../assets/images/brands/Chevrolet.svg'
+import Fiat from '../../../assets/images/brands/Fiat.svg'
+import Ford from '../../../assets/images/brands/Ford.svg'
+import Hyundai from '../../../assets/images/brands/Hyundai.svg'
+import KIA from '../../../assets/images/brands/KIA.svg'
+import Mercedes from '../../../assets/images/brands/Mercedes-Benz.svg'
+import Nissan from '../../../assets/images/brands/Nissan.svg'
+import Ssangyong from '../../../assets/images/brands/Ssangyong.svg'
+import Toyota from '../../../assets/images/brands/Toyota.svg'
+import Volkswagen from '../../../assets/images/brands/Volkswagen.svg'
+
 
 export const BrandCarrusel = () => {
-  const logos = new Array(10).fill(placeHolder);
+  const logos = [ Audi ,BMW, Chevrolet, Fiat, Ford,Hyundai, KIA, Mercedes,Nissan,Ssangyong, Toyota, Volkswagen,]
 
   return (
     <div className={styles.carrusel}>
--- src/components/sections/HowItWorks/HowItWorks.jsx
+++ src/components/sections/HowItWorks/HowItWorks.jsx
@@ -14,17 +14,21 @@ export const HowItWorks = () => {
       <div className={styles.steps}>
         <div className={styles.steps__one}>
           <div className={styles.content__text}>
-            <h1 className={styles.content__number}>1</h1>
-            <div className={styles.container__text}>
-              <h1 className={styles.container__text__title}>
-                Regístrate y verifica tu cuenta
-              </h1>
-              <p className={styles.container__text__paragraph}>
-                Únete a CARLO Subastas y accede al mejor stock de autos
-                verificados. Completa tu registro y verifica tu cuenta para
-                comenzar el proceso ¡Es gratis!
-              </p>
-              <a className={styles.container__text__anchor}>Regístrate ahora</a>
+            <div className={styles.text__container}>
+              <h1 className={styles.content__number}>1</h1>
+              <div className={styles.container__text}>
+                <h1 className={styles.container__text__title}>
+                  Regístrate y verifica tu cuenta
+                </h1>
+                <p className={styles.container__text__paragraph}>
+                  Únete a CARLO Subastas y accede al mejor stock de autos
+                  verificados. Completa tu registro y verifica tu cuenta para
+                  comenzar el proceso ¡Es gratis!
+                </p>
+                <a className={styles.container__text__anchor}>
+                  Regístrate ahora
+                </a>
+              </div>
             </div>
           </div>
           <div>
@@ -32,43 +36,50 @@ export const HowItWorks = () => {
           </div>
         </div>
         <div className={styles.steps__two}>
-          <div className={`${styles.content__text} $ ${styles.stepTwo__container__color}`}>
-            <h1
-              className={`${styles.content__number} ${styles.stepTwo__number__color}`}
-            >
-              2
-            </h1>
-            <div
-              className={styles.container__text}
-            >
+          <div
+            className={`${styles.content__text} $ ${styles.stepTwo__container__color}`}
+          >
+            <div className={styles.text__container}>
               <h1
-                className={`${styles.container__text__title} ${styles.stepTwo__title__color}`}
+                className={`${styles.content__number} ${styles.stepTwo__number__color}`}
               >
-                Agenda una inspección
+                2
               </h1>
-              <p
-                className={`${styles.container__text__paragraph} ${styles.stepTwo__paragraph__color}`}
-              >
-                Si estás de acuerdo con la cotización de tu auto, agenda una
-                inspección donde se le hará una revisión mecánica, estética y
-                legal. Esto nos permitirá confirmar la oferta y, si es posible,
-                hacer ajustes para reflejar su valor real.
-              </p>
+              <div className={styles.container__text}>
+                <h1
+                  className={`${styles.container__text__title} ${styles.stepTwo__title__color}`}
+                >
+                  Explora nuestro stock de vehículos
+                </h1>
+                <p
+                  className={`${styles.container__text__paragraph} ${styles.stepTwo__paragraph__color}`}
+                >
+                  Accede a nuestro catálogo de vehículos verificados con 180
+                  puntos de inspección y revisa fotos, estado mecánico, estético
+                  y legal antes de pujar. ¡Encuentra la mejor opción para tu
+                  negocio!
+                </p>
+              </div>
             </div>
           </div>
           <div>
-            <img src={stepTwoImg} />
+            <img src={stepTwoImg} className={styles.macImg} />
           </div>
         </div>
         <div className={styles.steps__one}>
           <div className={styles.content__text}>
-            <h1 className={styles.content__number}>3</h1>
-            <div className={styles.container__text}>
-              <h1 className={styles.container__text__title}>
-              Empieza a pujar desde cualquier dispositivo en tiempo real              </h1>
-              <p className={styles.container__text__paragraph}>
-              Desde tu celular, tablet o computadora, puja en tiempo real en las subastas. Haz tus ofertas al instante y ¡Recibirás una notificación cuando seas el ganador!
-              </p>
+            <div className={styles.text__container}>
+              <h1 className={styles.content__number}>3</h1>
+              <div className={styles.container__text}>
+                <h1 className={styles.container__text__title}>
+                  Empieza a pujar desde cualquier dispositivo en tiempo real{" "}
+                </h1>
+                <p className={styles.container__text__paragraph}>
+                  Desde tu celular, tablet o computadora, puja en tiempo real en
+                  las subastas. Haz tus ofertas al instante y ¡Recibirás una
+                  notificación cuando seas el ganador!
+                </p>
+              </div>
             </div>
           </div>
           <div>
@@ -76,26 +87,34 @@ export const HowItWorks = () => {
           </div>
         </div>
         <div className={styles.steps__two}>
-          <div className={`${styles.content__text} $ ${styles.stepTwo__container__color}`}>
-            <h1
-              className={`${styles.content__number} ${styles.stepTwo__number__color}`}
-            >
-              4
-            </h1>
-            <div
-              className={styles.container__text}
-            >
+          <div
+            className={`${styles.content__text} $ ${styles.stepTwo__container__color}`}
+          >
+            <div className={styles.text__container}>
               <h1
-                className={`${styles.container__text__title} ${styles.stepTwo__title__color}`}
+                className={`${styles.content__number} ${styles.stepTwo__number__color}`}
               >
-Completa la compra y recoge tu auto              </h1>
-              <p
-                className={`${styles.container__text__paragraph} ${styles.stepTwo__paragraph__color}`}
-              >
-               Si ganas la subasta, nuestro equipo se comunicará contigo para coordinar los siguientes pasos. Solo preocúpate por recoger tu auto y seguir con tu negocio.
-              </p>
-              <a className={`${styles.container__text__anchor} ${styles.stepTwo__anchor__color}`}>Regístrate ahora</a>
-
+                4
+              </h1>
+              <div className={styles.container__text}>
+                <h1
+                  className={`${styles.container__text__title} ${styles.stepTwo__title__color}`}
+                >
+                  Completa la compra y recoge tu auto{" "}
+                </h1>
+                <p
+                  className={`${styles.container__text__paragraph} ${styles.stepTwo__paragraph__color}`}
+                >
+                  Si ganas la subasta, nuestro equipo se comunicará contigo para
+                  coordinar los siguientes pasos. Solo preocúpate por recoger tu
+                  auto y seguir con tu negocio.
+                </p>
+                <a
+                  className={`${styles.container__text__anchor} ${styles.stepTwo__anchor__color}`}
+                >
+                  Regístrate ahora
+                </a>
+              </div>
             </div>
           </div>
           <div>
