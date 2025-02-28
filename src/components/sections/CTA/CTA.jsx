432ea7d7610eb99369bcb362d04f8cb144d9b799
David Palomino <david.leandro.palomino.rivera@gmail.com>
2025-02-28 00:20:16 -0500

    [Feat] CTA desing added

--- src/components/sections/CTA/CTA.jsx
+++ src/components/sections/CTA/CTA.jsx
@@ -3,13 +3,16 @@ import styles from "./CTA.module.css";
 export const CTA = () => {
   return (
     <div className={styles.CTAimg}>
-      <div className={styles.heroContent}>
-        <h1 className={styles.text}>
-          Empieza hoy y accede a los 
+      <div className={styles.CTA__content}>
+        <h1 className={styles.text}>Empieza hoy y accede a los</h1>
+        <h1 className={styles.text__jump}>
           <div className={styles.backLetters} />
           mejores autos en subasta
         </h1>
-        <p>Nuevo stock cada día para que nunca te falte inventario. Filtra, puja y gana las mejores unidades sin intermediarios ni riesgos.</p>
+        <p className={styles.text__paragraph}>
+          Nuevo stock cada día para que nunca te falte inventario. Filtra, puja
+          y gana las mejores unidades <b>sin intermediarios ni riesgos.</b>
+        </p>
         <button className={styles.beDealerButton}>Quiero ser dealer</button>
       </div>
     </div>
