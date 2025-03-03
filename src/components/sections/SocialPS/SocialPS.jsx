c3f9d90280dc9f5c40b6cf6857325937477995ab
David Palomino <david.leandro.palomino.rivera@gmail.com>
2025-03-03 12:06:26 -0500

    Intento 3

--- src/components/sections/SocialPS/SocialPS.jsx
+++ src/components/sections/SocialPS/SocialPS.jsx
@@ -15,7 +15,7 @@ export const SocialPS = ({windowSize}) => {
         <div className={styles.content__img}>
           <img src={socialImg} />
         </div>
-        {/* <div className={styles.content_text}>
+        <div className={styles.content_text}>
           <img src={windowSize > 768 ? purpleThing : prupleImgMobile} className={styles.prupleImg} />
           <div className={styles.container}>
             <div className={styles.text__container}>
@@ -34,7 +34,7 @@ export const SocialPS = ({windowSize}) => {
             </div>
             <button className={styles.beDealer}>Quiero ser dealer</button>
           </div>
-        </div> */}
+        </div>
       </div>
       <div>
       {/* <BrandCarrusel/> */}
