6f8ed8a475ca18e527efc7fbb94b9007606669b5
David Palomino <david.leandro.palomino.rivera@gmail.com>
2025-03-07 09:52:32 -0500

    Last changes

--- src/components/sections/CTA/CTA.jsx
+++ src/components/sections/CTA/CTA.jsx
@@ -5,16 +5,15 @@ export const CTA = ({windowSize}) => {
     <div className={styles.CTAimg}>
       {windowSize.width >= 500 ? (
       <div className={styles.CTA__content}>
-        <h1 className={styles.text}>Empieza hoy y accede a los</h1>
+        <h1 className={styles.text}>Vender tu auto</h1>
         <h1 className={styles.text__jump}>
           <div className={styles.backLetters} />
-          mejores autos en subasta
+          nunca fue tan fácil.
         </h1>
         <p className={styles.text__paragraph}>
-          Nuevo stock cada día para que nunca te falte inventario. Filtra, puja
-          y gana las mejores unidades <b>sin intermediarios ni riesgos.</b>
+        Consigue la mejor oferta en tiempo récord y sin complicaciones. Nuestra tecnología te garantiza un proceso rápido, seguro y sin estrés.
         </p>
-        <button className={styles.beDealerButton}>Quiero ser dealer</button>
+        <button className={styles.beDealerButton}>Quiero Vender</button>
       </div>
       ): (
         <div className={styles.CTA__content}>
--- src/components/sections/HowItWorks/HowItWorks.jsx
+++ src/components/sections/HowItWorks/HowItWorks.jsx
@@ -2,54 +2,21 @@ import styles from "./HowItWorks.module.css";
 import { useEffect, useRef, useState } from "react";
 
 import stepOneImg from "../../../assets/images/StepOne.svg";
-import stepTwoImg from "../../../assets/images/StepTwo.png";
+import stepTwoImg from "../../../assets/images/StepTwo.svg";
 import stepThreeImg from "../../../assets/images/StepThree.svg";
 import stepFourImg from "../../../assets/images/StepFour.svg";
 import checkIcon from "../../../assets/images/CheckIcon.svg";
 import socialVideo from "../../../assets/videos/SocialVideo.mp4";
 import stepFourDecoTop from "../../../assets/images/StepFourDecoTop.svg";
 import stepFourDecoBottom from "../../../assets/images/StepFourDeco.svg";
+
 export const HowItWorks = ({windowSize}) => {
-  const videoRef = useRef(null);
-  const [isMuted, setIsMuted] = useState(true);
 
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        if (entry.isIntersecting) {
-          setTimeout(() => {
-            videoRef.current.play().catch(error => console.error("Error al reproducir:", error));
-          }, 200);
-        } else {
-          videoRef.current.pause();
-        }
-      },
-      { threshold: 0.5 }
-    );
-  
-    if (videoRef.current) {
-      observer.observe(videoRef.current);
-    }
-  
-    return () => {
-      if (videoRef.current) {
-        observer.unobserve(videoRef.current);
-      }
-    };
-  }, []);
-
-
-  const handleToggleSound = () => {
-    if (videoRef.current) {
-      videoRef.current.muted = !videoRef.current.muted;
-      setIsMuted(videoRef.current.muted);
-    }
-  };
   return (
     <section className={styles.rectangle}>
       <div className={styles.header}>
         <div className={styles.header__squeare_text}>Nuestro proceso</div>
-        <h1 className={styles.header__title}>¿Cómo convertirte en dealer?</h1>
+        <h1 className={styles.header__title}>¿Cómo vender tu auto rápidamente?</h1>
       </div>
       <div className={styles.steps}>
         <div className={styles.steps__one}>
@@ -58,16 +25,14 @@ export const HowItWorks = ({windowSize}) => {
               <h1 className={styles.content__number}>1</h1>
               <div className={styles.container__text}>
                 <h1 className={styles.container__text__title}>
-                  Regístrate y verifica tu cuenta
+                  Cotiza tu vehículo
                 </h1>
                 <div className={styles.subcontainer__text}>
                 <p className={styles.container__text__paragraph}>
-                  Únete a CARLO Subastas y accede al mejor stock de autos
-                  verificados. Completa tu registro y verifica tu cuenta para
-                  comenzar el proceso <b>¡Es gratis!</b>
+                Valúa tu vehículo al instante con nuestra tecnología de valoración inteligente. Obtén el mejor precio de forma rápida, segura y sin complicaciones. <b>¡Hazlo! Te tomará menos de 2 minutos.</b>
                 </p>
                 <a className={styles.container__text__anchor}>
-                  Regístrate ahora
+                Quiero Cotizar mi Auto
                 </a>
                 </div>
               </div>
@@ -90,40 +55,35 @@ export const HowItWorks = ({windowSize}) => {
                 <h1
                   className={`${styles.container__text__title} ${styles.stepTwo__title__color}`}
                 >
-                  Explora nuestro{windowSize.width <= 500 ? <br/>:""} stock de vehículos
+                  Agenda una{windowSize.width > 500 ? <br/>:""} inspección
                 </h1>
                 <p
                   className={`${styles.container__text__paragraph} ${styles.stepTwo__paragraph__color}`}
                 >
-                  Accede a nuestro catálogo de vehículos verificados con 180
-                  puntos de inspección y revisa fotos, estado mecánico, estético
-                  y legal antes de pujar. <b>¡Encuentra la mejor opción para tu
-                  negocio!</b>
+                  Si estás de acuerdo con la cotización de tu auto, agenda una inspección donde se le hará una revisión mecánica, estética y legal. Esto nos permitirá confirmar la oferta y, si es posible, hacer ajustes para reflejar su valor real.
                 </p>
               </div>
             </div>
           </div>
           <div className={styles.step__img__container__two}>
-            <img src={stepTwoImg} className={styles.macImg} />
+            <img src={stepTwoImg}/>
           </div>
         </div>
         <div className={styles.steps__one}>
           <div className={styles.content__text}>
-            <div className={styles.text__container}>
+            <div className={`${styles.text__container} ${styles.container__text__three}`}>
               <h1 className={styles.content__number}>3</h1>
               <div className={styles.container__text}>
                 <h1 className={`${styles.container__text__title} ${styles.step__three__mobile__title}`}>
-                  Empieza a pujar desde cualquier dispositivo en tiempo real
+                  Inspeccionamos tu auto en solo 30 minutos
                 </h1>
                 <p className={styles.container__text__paragraph}>
-                  Desde tu celular, tablet o computadora, puja en tiempo real en
-                  las subastas. Haz tus ofertas al instante y ¡Recibirás una
-                  notificación cuando seas el ganador!
+                <b>¡Rápido, seguro y sin complicaciones!</b> Trae tu auto a nuestro centro de inspección en la fecha seleccionada y nuestro equipo de expertos revisará tu auto en 30 minutos aprox.
                 </p>
               </div>
             </div>
           </div>
-          <div className={styles.step__img__container}>
+          <div className={`${styles.step__img__container} ${styles.step__three__img}`}>
             <img src={stepThreeImg} />
           </div>
         </div>
@@ -142,20 +102,20 @@ export const HowItWorks = ({windowSize}) => {
                 <h1
                   className={`${styles.container__text__title} ${styles.stepTwo__title__color}`}
                 >
-                  Completa la compra y recoge tu auto
+                  Revisa las ofertas en tiempo real y completa la venta
                 </h1>
                 <div className={styles.subcontainer__text}>
                 <p
                   className={`${styles.container__text__paragraph} ${styles.stepTwo__paragraph__color}`}
                 >
-                  Si ganas la subasta, nuestro equipo se comunicará contigo para
-                  coordinar los siguientes pasos. Solo preocúpate por recoger tu
-                  auto y seguir con tu negocio.
+                  ¡Y listo! Haz el seguimiento de las pujas en tiempo real a través del link que te
+                  compartiremos. Al finalizar la subasta, podrás aceptar o rechazar la oferta. En ambos casos
+                  nuestro equipo se contactará contigo para coordinar los pasos finales.
                 </p>
                 <a
                   className={`${styles.container__text__anchor} ${styles.stepTwo__anchor__color}`}
                 >
-                  Quiero ser dealer
+                  Quiero Cotizar Ahora
                 </a>
               </div>
               </div>
@@ -165,38 +125,6 @@ export const HowItWorks = ({windowSize}) => {
           <div className={styles.step__img__container}>
             <img src={stepFourImg} />
           </div>
-        </div>
-      </div>
-      <div className={styles.user}>
-        <div className={styles.user__container}>
-          {windowSize.width >= 500 ?
-          <section className={styles.text__section}>
-            <div className={styles.text__content}>
-              <img src={checkIcon}/>
-              <div className={styles.text__subcontent}>
-              <h1 className={styles.text__title}>Te contamos cómo <br/> comprar con total<br/> seguridad</h1>
-              <p className={styles.text__paragraph}>Aprende en minutos cómo registrarte, explorar el stock de vehículos y participar en subastas en tiempo real.</p>
-              </div>
-              <p className={styles.text__paragraph}>Descubre lo fácil y seguro que es comprar autos verificados para tu negocio.</p>
-            </div>
-            <button className={styles.beDealer}>Quiero ser dealer</button>
-          </section>
-           : ""}
-           {windowSize.width <= 500 ? 
-           <h1 className={styles.video__title}>Te contamos cómo comprar con total seguridad</h1>
-            : ""}
-          <section className={styles.video__container} onClick={handleToggleSound}>
-              <video className={styles.video}
-                ref={videoRef}
-                playsInline
-                muted
-                loop>
-                <source src={socialVideo} type="video/mp4" />
-              </video>
-          </section>
-          {windowSize.width <= 500 ? 
-           <button className={styles.beDealer}>Quiero ser dealer</button>
-            : ""}
         </div>
       </div>
     </section>
